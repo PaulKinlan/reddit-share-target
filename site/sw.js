a830b00e91484115e22dd232f1d30c5d1107b787
Paul Kinlan <paul.kinlan@gmail.com>
2019-11-24 18:52:55 +0000

    First commit

---
+++ site/sw.js
@@ -0,0 +1,18 @@
+onfetch = async (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname !== '/share.html') return;
+
+  const location = encodeURIComponent(url.searchParams.get('url') || url.searchParams.get('text'));
+  const title = encodeURIComponent(url.searchParams.get('title'))
+  const hnUrl = `http://www.reddit.com/submit?url=${location}&title=${title}`
+
+  event.waitUntil(event.respondWith(Response.redirect(hnUrl)));
+};
+
+oninstall = () => {
+  skipWaiting();
+};
+
+onactivate = () => {
+  clients.claim();
+};
